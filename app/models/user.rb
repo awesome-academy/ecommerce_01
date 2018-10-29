4f1c2d46c26c3d77a69bd061f4be5852c78ae4cb
Vu Quang Thai <vuquangthai2201@gmail.com>
2018-10-29 13:27:48 +0700

    sign_up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  before_save{self.email = email.downcase}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   has_many :raitings
   has_many :comments
@@ -9,4 +11,22 @@ class User < ApplicationRecord
   validates :name, presence: true
   has_secure_password
   enum role: {customer: 0, admin: 1}
+
+  def self.digest string
+    cost =  if ActiveModel::SecurePassword.min_cost
+              BCrypt::Engine::MIN_COST
+            else
+              BCrypt::Engine.cost
+            end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 end
