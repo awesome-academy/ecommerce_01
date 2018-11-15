04115063127a9d7c7bb23465439df37015eb8009
dtmhdev89 <dtmhqng2906@gmail.com>
2018-11-15 23:09:13 +0700

    change carts controller, add cart_items controller

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -2,11 +2,19 @@ $(document).on("change", ".quantity-in-cart", function(event) {
   event.preventDefault();
   let element = event.target
     $.ajax({
-        url: "/cart",
-        method: "put",
-        data: {
-          product_id: element.offsetParent.id,
-          quantity: element.value
+      url: "/cart_item",
+      method: "patch",
+      data: {
+        product_id: element.offsetParent.id,
+        quantity: element.value
+      },
+      success: function(data) {
+        if(data.errors != undefined) {
+          alert(I18n.t("js.cart.error_update",{error: data.errors}));
+        }
+      },
+      error: function(data){
+        alert(data.error);
       }
     });
 });
@@ -16,14 +24,16 @@ $(document).on("click", ".btn-remove", function(event) {
   let el = event.target;
   var id = el.parentElement.previousElementSibling.previousElementSibling.id;
   $.ajax({
-    url: "/cart",
+    url: "/cart_item",
     method: "delete",
     data: {
       product_id: id
     },
     success: function(){
         el.parentElement.parentElement.remove();
-        location.reload();
+    },
+    error: function(data){
+      alert(data.error);
     }
   });
 });
